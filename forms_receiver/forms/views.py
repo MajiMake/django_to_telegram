94d5381f3f7ad83e49f3d89a16747d6106b9254b
MajiMake <wiz33@yandex.ru>
2023-04-24 09:40:35 +0300

    add aiogram

--- forms_receiver/forms/views.py
+++ forms_receiver/forms/views.py
@@ -2,24 +2,16 @@ import requests
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json as js
+import aiogram
 
 
-def telegram(form):
+async def telegram(form):
 
     token = 'token'
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    payload = {
-        "chat_id": 'chat_id',
-        "text": f"{form}",
-    }
-    # Send the POST request to the Telegram bot API
-    response = requests.post(url, data=payload)
+    bot: aiogram.Bot = aiogram.Bot(token)
+    await bot.send_message('chat_id', form)
 
-    # Check the response status code
-    if response.status_code == 200:
-        return "Message sent successfully."
-    else:
-        return "Failed to send message."
+
 @csrf_exempt
 def handler(request):
     text = ''
