d8ccec84f5756d7d4b0ca1e1892c0db43a18c7a1
Maji <wiz33@yandex.ru>
2023-06-13 13:39:11 +0300

    v2.1

--- forms_receiver/forms/views.py
+++ forms_receiver/forms/views.py
@@ -1,24 +1,28 @@
 import requests
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json as js
-import aiogram
-import asyncio
 
 
-async def telegram(form):
+def telegram(form):
+    token = '1759600278:AAGWpC48yFaSz_YbFXOjDcqAPX6vkeIIPJk'
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {
+        "chat_id": '-1001961235572',
+        "text": f"{form}",
+    }
+    response = requests.post(url, data=payload)
 
-    token = 'token'
-    bot: aiogram.Bot = aiogram.Bot(token)
-    await bot.send_message('chat_id', form)
+    if response.status_code == 200:
+        return HttpResponse("Message sent successfully.")
+    else:
+        return HttpResponse("Failed to send message.")
 
 
 @csrf_exempt
 def handler(request):
-    print('hih')
     text = ''
     data: dict = js.loads(request.body)['params']
-    for key, value in data.items():
+    for key, value in data.items():  # reversed(data.items()):
         text += f'{key}\n{value}\n\n'
-    asyncio.run(telegram(text))
-    return HttpResponse('OK')
+    return telegram(text)
